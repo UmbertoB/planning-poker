77b94982e905424d35f66b7858e712d0498b96a2
UmbertoB <umbertobarrosf@gmail.com>
2020-06-23 16:47:51 -0300

    deleting package-lock

--- packages/frontend/src/environment/index.ts
+++ packages/frontend/src/environment/index.ts
@@ -5,7 +5,7 @@ let url;
 if (REACT_APP_ENV === 'production') {
     url = 'http://18.216.8.21';
 } else {
-    url = 'http://192.168.0.25:8000';
+    url = 'http://localhost:8000';
 }
 
 export const env = REACT_APP_ENV;
